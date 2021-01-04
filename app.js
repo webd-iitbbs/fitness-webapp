7ebc0c0f7d066e0e71bca7fac66d459ed9498379
NEMESIS <61878627+JOY-noob@users.noreply.github.com>
2021-01-05 03:01:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(fileUpload());
- app.listen(process.env.PORT || 3000, function(){
+ app.listen(process.env.PORT || 3000,"0.0.0.0", function(){
    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
  });
 
