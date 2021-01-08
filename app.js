20d6451222e7e61a426a1cdd916ffdd8c48c863c
NEMESIS <61878627+JOY-noob@users.noreply.github.com>
2021-01-08 11:37:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -252,8 +252,8 @@ MongoClient.connect("mongodb+srv://su123:su123@cluster0.imrnk.mongodb.net/db?ret
           update.$set[lastupdates] = s;
               update.$set[lastupdatesd] = ad + parseFloat(req.body.val);
               update.$set[lastupdatest] = at + parseFloat(req.body.valt);
-          update.$set[s] = (parseFloat(req.body.val)/parseFloat(req.body.valt)).toFixed(3);
-          update.$set[section] = ((ad + parseFloat(req.body.val))/(at + parseFloat(req.body.valt))).toFixed(3);
+          update.$set[s] = (parseFloat(req.body.val)/parseFloat(req.body.valt));
+          update.$set[section] = ((ad + parseFloat(req.body.val))/(at + parseFloat(req.body.valt)));
           console.log(update);
         }})}
         else if (req.body.section == 'running'){db.collection('users').find({'email':req.user.email,'lastupdater':s}).count()
@@ -264,8 +264,8 @@ MongoClient.connect("mongodb+srv://su123:su123@cluster0.imrnk.mongodb.net/db?ret
           update.$set[lastupdater] = s;
               update.$set[lastupdaterd] = ad + parseFloat(req.body.val);
               update.$set[lastupdatert] = at + parseFloat(req.body.valt);
-          update.$set[s] = (parseFloat(req.body.val)/parseFloat(req.body.valt)).toFixed(3);
-          update.$set[section] = ((ad + parseFloat(req.body.val))/(at + parseFloat(req.body.valt))).toFixed(3);
+          update.$set[s] = (parseFloat(req.body.val)/parseFloat(req.body.valt));
+          update.$set[section] = ((ad + parseFloat(req.body.val))/(at + parseFloat(req.body.valt)));
           console.log(update);
         }})}
         else if (req.body.section == 'cycling'){db.collection('users').find({'email':req.user.email,'lastupdatec':s}).count()
@@ -276,8 +276,8 @@ MongoClient.connect("mongodb+srv://su123:su123@cluster0.imrnk.mongodb.net/db?ret
          update.$set[lastupdatec] = s;
                update.$set[lastupdatecd] = ad + parseFloat(req.body.val);
               update.$set[lastupdatect] = at + parseFloat(req.body.valt);
-         update.$set[s] = (parseFloat(req.body.val)/parseFloat(req.body.valt)).toFixed(3);
-          update.$set[section] = ((ad + parseFloat(req.body.val))/(at + parseFloat(req.body.valt))).toFixed(3);
+         update.$set[s] = (parseFloat(req.body.val)/parseFloat(req.body.valt));
+          update.$set[section] = ((ad + parseFloat(req.body.val))/(at + parseFloat(req.body.valt)));
           console.log(update);
         }})}
         
